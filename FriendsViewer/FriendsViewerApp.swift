a529f20ab7fdcb2fc89495e0b9f1e6af281b0db7
ZhichGaming <nick.zhicheng@gmail.com>
2022-09-20 16:29:43 -0400

    Initial Commit

---
+++ FriendsViewer/FriendsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsViewerApp.swift
+//  FriendsViewer
+//
+//  Created by Nick on 2022-09-20.
+//
+
+import SwiftUI
+
+@main
+struct FriendsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
